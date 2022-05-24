4b9f17ab4ac5b51e0198ea0e97c03c72d7734d2a
shinqn <78954375+shinqn@users.noreply.github.com>
2022-05-25 06:59:46 +0700

    Add files via upload

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,35 +1,13 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom"
 
 function Nav() {
-    const clickButton = () => {
-        console.log(document.URL);
-    };
-    return (
-        <>
-            <nav>
-                <li>
-                    <Link to="/" onClick={clickButton}>
-                        Home
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/following" onClick={clickButton}>
-                        Folowing
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/profile" onClick={clickButton}>
-                        Profile
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/upload" onClick={clickButton}>
-                        Upload
-                    </Link>
-                </li>
-            </nav>
-        </>
-    );
-}
-
-export default Nav;
+    return <>
+      <nav>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </nav>
+    </>
+  
+  }
+  
+  export default Nav
