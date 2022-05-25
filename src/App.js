c2d5f41e5cc5da6f0666d4d2a2cb28dab1f2718b
shinqn <78954375+shinqn@users.noreply.github.com>
2022-05-25 07:00:55 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import { Routes, Route } from "react-router-dom"
-import Home from "./components/Home"
-import About from "./components/About"
-import Nav from "./components/Nav"
+// import { Routes, Route } from 'react-router-dom';
+// import Home from './components/Home';
+// import About from './components/About';
+// import Nav from './components/Nav';
+import Router from './routes';
+
+// theme
+import ThemeProvider from './theme';
 
 function App() {
-  return <>
-    <Nav />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="about" element={<About />} />
-    </Routes>
-  </>
-
+    return (
+        <ThemeProvider>
+            <Router />
+        </ThemeProvider>
+    );
 }
 
-export default App
+export default App;
--- src/layouts/DefaultLayout/Header.js
+++ src/layouts/DefaultLayout/Header.js
@@ -1,8 +1,47 @@
-function Header() {
+import PropTypes from 'prop-types';
+// material
+import { alpha, styled } from '@mui/material/styles';
+import { Box, AppBar, Toolbar, IconButton } from '@mui/material';
+// components
+import Iconify from '../../components/Iconify';
+//
+// ----------------------------------------------------------------------
+
+const DRAWER_WIDTH = 280;
+const APPBAR_MOBILE = 64;
+const APPBAR_DESKTOP = 92;
+
+const RootStyle = styled(AppBar)(({ theme }) => ({
+    boxShadow: 'none',
+    backdropFilter: 'blur(6px)',
+    WebkitBackdropFilter: 'blur(6px)', // Fix on Mobile
+    backgroundColor: alpha(theme.palette.background.default, 0.72),
+    [theme.breakpoints.up('lg')]: {
+        width: `calc(100% - ${DRAWER_WIDTH + 1}px)`,
+    },
+}));
+
+const ToolbarStyle = styled(Toolbar)(({ theme }) => ({
+    minHeight: APPBAR_MOBILE,
+    [theme.breakpoints.up('lg')]: {
+        minHeight: APPBAR_DESKTOP,
+        padding: theme.spacing(0, 5),
+    },
+}));
+
+Header.propTypes = {
+    onOpenSidebar: PropTypes.func,
+};
+function Header({ onOpenSidebar }) {
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <RootStyle>
+            <ToolbarStyle>
+                <IconButton onClick={onOpenSidebar} sx={{ mr: 1, color: 'text.primary', display: { lg: 'none' } }}>
+                    <Iconify icon="eva:menu-2-fill" />
+                </IconButton>
+                <Box sx={{ flexGrow: 1 }} />
+            </ToolbarStyle>
+        </RootStyle>
     );
 }
 
